9ac866a0f3a0eeed827508f21bf77b775efc4802
Adithya M S <adithyashivaram@gmail.com>
2021-09-08 10:11:57 +0530

    Fixed Issue with font case

--- Assignment_1/Task_1/mapper.py
+++ Assignment_1/Task_1/mapper.py
@@ -11,12 +11,12 @@ def isNotNan(data):
 
 def filterBasedOnConstraints(data):
     #{"ID": "A-1655975", "Severity": 3, "Start_Time": "2017-02-07 13:32:33", "End_Time": "2017-02-07 14:17:05", "Start_Lat": 42.385605, "Start_Lng": -83.15898100000001, "End_Lat": NaN, "End_Lng": NaN, "Distance(mi)": 0.01, "Description": "Left and left center lane blocked and left hand shoulder blocked due to accident on I-96 Eastbound at Exits 186A 186B Wyoming St.", "Number": NaN, "Street": "Wyoming St", "Side": "R", "Country": "US", "Timezone": "US/Eastern", "Airport_Code": "KDET", "Weather_Timestamp": "2017-02-07 13:38:00", "Temperature(F)": 36.0, "Wind_Chill(F)": 29.6, "Humidity(%)": 100.0, "Pressure(in)": 29.37, "Visibility(mi)": 3.0, "Wind_Direction": "ESE", "Wind_Speed(mph)": 8.1, "Precipitation(in)": 0.06, "Weather_Condition": "Light Rain", "Amenity": false, "Bump": false, "Crossing": false, "Give_Way": false, "Junction": false, "No_Exit": false, "Railway": false, "Roundabout": false, "Station": false, "Stop": false, "Traffic_Calming": false, "Traffic_Signal": false, "Turning_Loop": false, "Sunrise_Sunset": "Day", "Civil_Twilight": "Day", "Nautical_Twilight": "Day", "Astronomical_Twilight": "Day"}
-    if "lane blocked" in data["Description"] or "shoulder blocked" in data["Description"] or "overturned vehicle" in data["Description"]:
+    if "lane blocked" in data["Description"].lower() or "shoulder blocked" in data["Description"].lower() or "overturned vehicle" in data["Description"].lower():
         if data["Severity"] >=2:
             if data["Sunrise_Sunset"] == "Night":
                 if data["Visibility(mi)"]<=10.0:
                     if data["Precipitation(in)"]>=0.2:
-                        if data["Weather_Condition"] == "Heavy Snow" or  data["Weather_Condition"] == "Thunderstorm" or data["Weather_Condition"] ==  "Heavy Rain" or data["Weather_Condition"] ==  "Heavy Rain Showers" or data["Weather_Condition"] == "Blowing Dust":
+                        if data["Weather_Condition"].lower()  == "heavy snow" or  data["Weather_Condition"].lower() == "thunderstorm" or data["Weather_Condition"].lower()  ==  "heavy rain" or data["Weather_Condition"].lower()  ==  "heavy rain showers" or data["Weather_Condition"].lower()  == "blowing dust":
                             return True
     return False
 
@@ -36,4 +36,5 @@ for line in sys.stdin:
             #hour=datetime.strftime(dateTimeObject,"%H")
             hour=time.strftime(dateTimeObject,"%H")
             #hour=data["Start_Time"].hour"""
+            hour=int(hour)
             print(hour,"\t1")
