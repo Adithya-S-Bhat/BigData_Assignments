a84375352e8915783869fae98cdce96b5a777f34
Adithya M S <adithyashivaram@gmail.com>
2021-09-08 15:03:51 +0530

    Completed Task1

--- Assignment_1/Task_1/readme.md
+++ Assignment_1/Task_1/readme.md
@@ -1,3 +1,5 @@
 Command to execute mapper.py locally:
 cat ../US_ACCIDENT_DATA_5PERCENT.json | python3 mapper.py | sort -k1,1
 
+Command to execute MR task locally:
+cat ../US_ACCIDENT_DATA_5PERCENT.json | python3 mapper.py | sort -k1,1 | python3 reducer.py
--- Assignment_1/Task_1/reducer.py
+++ Assignment_1/Task_1/reducer.py
@@ -1,7 +1,22 @@
+#!/usr/bin/env python
 import sys
+
+countDict=dict()
 
 for line in sys.stdin:
     line = line.strip()
-    date, count = line.split("\t")
+    hour, count = line.split("\t")
 
-    
+    try:
+        count=int(count)
+        hour=int(hour)
+    except ValueError:
+        continue
+
+    if hour in countDict.keys():
+        countDict[hour]+=count
+    else:
+        countDict[hour]=count
+
+for hour,count in countDict.items():
+    print(hour,"\t",count)
