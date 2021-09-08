4389f51b33516f17a07036e23a31ad91e5434d09
Adithya M S <adithyashivaram@gmail.com>
2021-09-08 14:48:26 +0530

    Fixed issue with datetime in mapper

--- Assignment_1/Task_1/mapper.py
+++ Assignment_1/Task_1/mapper.py
@@ -10,7 +10,14 @@ def isNotNan(data):
     return True
 
 def filterBasedOnConstraints(data):
-    #{"ID": "A-1655975", "Severity": 3, "Start_Time": "2017-02-07 13:32:33", "End_Time": "2017-02-07 14:17:05", "Start_Lat": 42.385605, "Start_Lng": -83.15898100000001, "End_Lat": NaN, "End_Lng": NaN, "Distance(mi)": 0.01, "Description": "Left and left center lane blocked and left hand shoulder blocked due to accident on I-96 Eastbound at Exits 186A 186B Wyoming St.", "Number": NaN, "Street": "Wyoming St", "Side": "R", "Country": "US", "Timezone": "US/Eastern", "Airport_Code": "KDET", "Weather_Timestamp": "2017-02-07 13:38:00", "Temperature(F)": 36.0, "Wind_Chill(F)": 29.6, "Humidity(%)": 100.0, "Pressure(in)": 29.37, "Visibility(mi)": 3.0, "Wind_Direction": "ESE", "Wind_Speed(mph)": 8.1, "Precipitation(in)": 0.06, "Weather_Condition": "Light Rain", "Amenity": false, "Bump": false, "Crossing": false, "Give_Way": false, "Junction": false, "No_Exit": false, "Railway": false, "Roundabout": false, "Station": false, "Stop": false, "Traffic_Calming": false, "Traffic_Signal": false, "Turning_Loop": false, "Sunrise_Sunset": "Day", "Civil_Twilight": "Day", "Nautical_Twilight": "Day", "Astronomical_Twilight": "Day"}
+    #Example of a Record: {"ID": "A-1655975", "Severity": 3, "Start_Time": "2017-02-07 13:32:33", "End_Time": "2017-02-07 14:17:05", "Start_Lat": 42.385605, 
+    # "Start_Lng": -83.15898100000001, "End_Lat": NaN, "End_Lng": NaN, "Distance(mi)": 0.01, "Description": "Left and left center lane blocked 
+    # and left hand shoulder blocked due to accident on I-96 Eastbound at Exits 186A 186B Wyoming St.", "Number": NaN, "Street": "Wyoming St", 
+    # "Side": "R", "Country": "US", "Timezone": "US/Eastern", "Airport_Code": "KDET", "Weather_Timestamp": "2017-02-07 13:38:00", "Temperature(F)": 36.0, 
+    # "Wind_Chill(F)": 29.6, "Humidity(%)": 100.0, "Pressure(in)": 29.37, "Visibility(mi)": 3.0, "Wind_Direction": "ESE", "Wind_Speed(mph)": 8.1, "Precipitation(in)": 0.06, 
+    # "Weather_Condition": "Light Rain", "Amenity": false, "Bump": false, "Crossing": false, "Give_Way": false, "Junction": false, "No_Exit": false, "Railway": false, "Roundabout": false, 
+    # "Station": false, "Stop": false, "Traffic_Calming": false, "Traffic_Signal": false, "Turning_Loop": false, "Sunrise_Sunset": "Day", "Civil_Twilight": "Day", "Nautical_Twilight": "Day", "Astronomical_Twilight": "Day"}
+
     if "lane blocked" in data["Description"].lower() or "shoulder blocked" in data["Description"].lower() or "overturned vehicle" in data["Description"].lower():
         if data["Severity"] >=2:
             if data["Sunrise_Sunset"] == "Night":
@@ -25,16 +32,12 @@ for line in sys.stdin:
     data = json.loads(line)
     if(isNotNan(data)):
         if(filterBasedOnConstraints(data)):
-            #2017-02-07 13:32:33.354
-            hour=str(data["Start_Time"])[11:13]
-            """try:
-                dateTimeObject=datetime.strptime(str(data["Start_Time"]).strip(),"%Y-%m-%d %H:%M:%S.%f")
+            #Example of time Format;"2017-02-07 13:32:33.354142"
+            #hour=str(data["Start_Time"])[11:13]
+            try:
+                dateTimeObject=datetime.strptime(str(data["Start_Time"]).strip()[:26],"%Y-%m-%d %H:%M:%S.%f")
             except ValueError:
-                print(data["Start_Time"])
                 dateTimeObject=datetime.strptime(str(data["Start_Time"]).strip(),"%Y-%m-%d %H:%M:%S")
             
-            #hour=datetime.strftime(dateTimeObject,"%H")
-            hour=time.strftime(dateTimeObject,"%H")
-            #hour=data["Start_Time"].hour"""
-            hour=int(hour)
+            hour=datetime.strftime(dateTimeObject,"%H")
             print(hour,"\t1")
---
+++ Assignment_1/Task_1/readme.md
@@ -0,0 +1,3 @@
+Command to execute mapper.py locally:
+cat ../US_ACCIDENT_DATA_5PERCENT.json | python3 mapper.py | sort -k1,1
+
