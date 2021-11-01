3123b096c9dcc8397fbf51b259e32fe53492ecd9
Adithya M S <adithyashivaram@gmail.com>
2021-11-01 15:24:39 +0530

    A3 T2 fixed

--- A3/task2.py
+++ A3/task2.py
@@ -10,17 +10,13 @@ spark_context = SparkContext.getOrCreate()
 sqlContext=SQLContext(spark_context)
 
 df=sqlContext.read.csv(cityPath,header = True,inferSchema=True)
-df.AverageTemperature=df.AverageTemperature.cast(FloatType())
 citydf=df.dropna(how='any',subset=['AverageTemperature','dt'])
-#citydf.withColumn("AverageTemperature",df.AverageTemperature.cast('float'))
-#citydf.AverageTemperature=citydf.AverageTemperature.cast(FloatType())
+citydf.AverageTemperature=citydf.AverageTemperature.cast(FloatType())
 
 df=sqlContext.read.csv(globalPath,header = True,inferSchema=True)
-df.LandAverageTemperature=df.LandAverageTemperature.cast(FloatType())
 globaldf=df.dropna(how='any',subset=['LandAverageTemperature','dt'])
 globaldf=globaldf.select("dt","LandAverageTemperature")
-#globaldf.withColumn("LandAverageTemperature",df.LandAverageTemperature.cast('float'))
-#globaldf.LandAverageTemperature=globaldf.LandAverageTemperature.cast(FloatType())
+globaldf.LandAverageTemperature=globaldf.LandAverageTemperature.cast(FloatType())
 
 groupedDf=citydf.groupby("dt","Country").max("AverageTemperature")
 groupedDf=groupedDf.withColumnRenamed("max(AverageTemperature)","max")
@@ -32,7 +28,7 @@ joinDf=groupedDf.join(globaldf,globaldf.date==groupedDf.dt,how="inner")
 
 joinDf.LandAverageTemperature=joinDf.LandAverageTemperature.cast(FloatType())
 joinDf.max=joinDf.max.cast(FloatType())
-resultDf=joinDf.filter(joinDf['max'] > joinDf['LandAverageTemperature']).groupBy("Country").count().orderBy("Country")
+resultDf=joinDf.filter(joinDf.max > joinDf.LandAverageTemperature).groupBy("Country").count().sort("Country")
 
 result=resultDf.collect()
 
