3ba76819c58f3b6f7b25d3c3e36a1194719ab84f
Adithya M S <adithyashivaram@gmail.com>
2021-11-01 13:39:57 +0530

    Made some changes

--- A3/task2.py
+++ A3/task2.py
@@ -10,23 +10,29 @@ spark_context = SparkContext.getOrCreate()
 sqlContext=SQLContext(spark_context)
 
 df=sqlContext.read.csv(cityPath,header = True,inferSchema=True)
-citydf=df.dropna(how='any',subset=['AverageTemperature'])
+df.AverageTemperature=df.AverageTemperature.cast(FloatType())
+citydf=df.dropna(how='any',subset=['AverageTemperature','dt'])
 #citydf.withColumn("AverageTemperature",df.AverageTemperature.cast('float'))
-citydf.AverageTemperature=citydf.AverageTemperature.cast(FloatType())
+#citydf.AverageTemperature=citydf.AverageTemperature.cast(FloatType())
 
 df=sqlContext.read.csv(globalPath,header = True,inferSchema=True)
-globaldf=df.dropna(how='any',subset=['LandAverageTemperature'])
+df.LandAverageTemperature=df.LandAverageTemperature.cast(FloatType())
+globaldf=df.dropna(how='any',subset=['LandAverageTemperature','dt'])
 globaldf=globaldf.select("dt","LandAverageTemperature")
 #globaldf.withColumn("LandAverageTemperature",df.LandAverageTemperature.cast('float'))
-globaldf.AverageTemperature=globaldf.LandAverageTemperature.cast(FloatType())
+#globaldf.LandAverageTemperature=globaldf.LandAverageTemperature.cast(FloatType())
 
 groupedDf=citydf.groupby("dt","Country").max("AverageTemperature")
 groupedDf=groupedDf.withColumnRenamed("max(AverageTemperature)","max")
 
 globaldf=globaldf.withColumnRenamed("dt","date")
+globaldf.date=globaldf.date.cast(DateType())
+groupedDf.dt=groupedDf.dt.cast(DateType())
 joinDf=groupedDf.join(globaldf,globaldf.date==groupedDf.dt,how="inner")
 
-resultDf=joinDf.filter(joinDf['max']>joinDf['LandAverageTemperature']).groupBy("Country").count().orderBy("Country")
+joinDf.LandAverageTemperature=joinDf.LandAverageTemperature.cast(FloatType())
+joinDf.max=joinDf.max.cast(FloatType())
+resultDf=joinDf.filter(joinDf['max'] > joinDf['LandAverageTemperature']).groupBy("Country").count().orderBy("Country")
 
 result=resultDf.collect()
 
